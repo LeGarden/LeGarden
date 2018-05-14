599f484a82d5aadbe80c9f467d633aeb4c20097d
agp <teddfan@gmail.com>
2018-05-14 22:31:24 +0200

    removed node-cmd

--- src/Infrastructure/MockNetworkController.ts
+++ src/Infrastructure/MockNetworkController.ts
@@ -1,4 +1,3 @@
-import cmd = require('node-cmd');
 import { INetworkController } from './INetworkController';
 
 export class MockNetworkController implements INetworkController {
@@ -7,30 +6,14 @@ export class MockNetworkController implements INetworkController {
     console.log(
       'pretending setting up modem, trying to exec cmd: echo connect modem'
     );
-    cmd.get('echo connect modem', (err: any, data: any, stderr: any) => {
-      if (!err) {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned:\n\n', data);
-      } else {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned error:\n\n', err);
-      }
-    });
+    
   }
   public connect(): void {
     // tslint:disable-next-line:no-console
     console.log(
       'pretending connecting umts, trying to exec cmd: echo connect umts'
     );
-    cmd.get('echo connect umts', (err: any, data: any, stderr: any) => {
-      if (!err) {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned:\n\n', data);
-      } else {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned error:\n\n', err);
-      }
-    });
+    
   }
 
   public disconnect(): void {
@@ -38,14 +21,6 @@ export class MockNetworkController implements INetworkController {
     console.log(
       'pretending disconnecting umts, trying to exec cmd: echo disconnect umts'
     );
-    cmd.get('echo disconnect umts', (err: any, data: any, stderr: any) => {
-      if (!err) {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned:\n\n', data);
-      } else {
-        // tslint:disable-next-line:no-console
-        console.log('the cmd returned error:\n\n', err);
-      }
-    });
+   
   }
 }
